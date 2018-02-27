e7c540f2fbc27cddd86ceeefc1bef0e833f244aa
Vikas Singh <vsvikassingh49@gmail.com>
2018-02-28 00:16:52 +0530

    Added Source

---
+++ MyMVCProjectSolution/MyMVCProject.Business/ApplicationFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMVCProject.BusinessObject;
+
+namespace MyMVCProject.Business
+{
+    public class ApplicationFacade : IApplicationFacade
+    {
+
+    }
+}
